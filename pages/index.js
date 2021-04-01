d6afa0c8da5c034029015c7c7f68bc8c40d8a937
Lucas Maidana <lucas_sknr.010@live.com>
2021-04-01 17:19:36 -0300

    added helper and the first paginator

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import { getTypeColor } from "../helper";
 
 const PokedexContainer = styled.div`
   width: 90vw;
@@ -101,56 +102,28 @@ const TypeContainer = styled.div`
 `;
 
 const index = ({ pokemons }) => {
-  const getTypeColor = (type) => {
-    switch (type) {
-      case "fire":
-        return "#fba14d";
-      case "water":
-        return "#0875c8";
-      case "normal":
-        return "#9f9fa0";
-      case "grass":
-        return "#5cbf6c";
-      case "electric":
-        return "#f6de61";
-      case "ice":
-        return "#81d6ca";
-      case "ground":
-        return "#d97d4d";
-      case "flying":
-        return "#97acdf";
-      case "ghost":
-        return "#6c70c9";
-      case "rock":
-        return "#cec08d";
-      case "fighting":
-        return "#e04551";
-      case "poison":
-        return "#bc66d1";
-      case "psychic":
-        return "#fb9088";
-      case "bug":
-        return "#a6c53f";
-      case "dark":
-        return "#676971";
-      case "steel":
-        return "#5595a5";
-      case "dragon":
-        return "#0875c8";
-      case "fairy":
-        return "#f09ce6";
-    }
-  };
-  const router = useRouter();
+  //states del paginador
+  const [page, setPage] = useState(1);
+  const [nextpage, setNextPage] = useState(0);
+  const [prevpage, setPrevPage] = useState(20);
+  //states del query
+  const [query, setQuery] = useState(0);
+  let offset = 20;
 
+  useEffect(() => {
+    setNextPage(page * offset);
+    setPrevPage(nextpage - offset );
+    console.log("offset siguiente", nextpage);
+    console.log("pffset anterior", prevpage);
+  }, [nextpage, prevpage, page]);
+
+  const router = useRouter();
   return (
     <PokedexContainer>
       {React.Children.toArray(
         pokemons.map((pokemon) => (
           <PokemonItem
-            onClick={()=>(
-              router.push(`/pokemon/${pokemon.id}`)
-            )}
+            onClick={() => router.push(`/pokemon/${pokemon.id}`)}
             background={() => getTypeColor(pokemon.types[0].type.name)}
           >
             <PokemonImg
@@ -172,12 +145,32 @@ const index = ({ pokemons }) => {
           </PokemonItem>
         ))
       )}
+      <div className="button_container">
+        <button
+          onClick={() => {
+            router.push({ pathname: "/", query: { page: prevpage } });
+            setPage(page - 1);
+          }}
+        >
+          Anterior
+        </button>
+        <button
+          onClick={() => {
+            setPage(page + 1);
+            router.push({ pathname: "/", query: { page: nextpage } });
+          }}
+        >
+          Siguiente
+        </button>
+      </div>
     </PokedexContainer>
   );
 };
-export async function getServerSideProps(context) {
+export async function getServerSideProps({ query }) {
+  console.log(query.page);
+
   const pokemonsList = await fetch(
-    "https://pokeapi.co/api/v2/pokemon/?offset=0"
+    `https://pokeapi.co/api/v2/pokemon/?offset=${query.page}`
   );
   const pokemonsJSON = await pokemonsList.json();
   const pokemonsData = await Promise.all(
--- pages/pokemon/[...slug].js
+++ pages/pokemon/[...slug].js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import { useRouter } from "next/router";
 import Head from 'next/head';
 import { IoMdArrowRoundBack } from 'react-icons/io';
+import { getTypeColor } from '../../helper';
 
 const Container = styled.div`
   height: 80vh;
@@ -123,47 +124,7 @@ const StatValue = styled(StatBase)`
   }
 `;
 const PokemonPage = (props) => {
-  //console.log(props);
-  const getTypeColor = (type) => {
-    switch (type) {
-      case "fire":
-        return "#fba14d";
-      case "water":
-        return "#0875c8";
-      case "normal":
-        return "#9f9fa0";
-      case "grass":
-        return "#5cbf6c";
-      case "electric":
-        return "#f6de61";
-      case "ice":
-        return "#81d6ca";
-      case "ground":
-        return "#d97d4d";
-      case "flying":
-        return "#97acdf";
-      case "ghost":
-        return "#6c70c9";
-      case "rock":
-        return "#cec08d";
-      case "fighting":
-        return "#e04551";
-      case "poison":
-        return "#bc66d1";
-      case "psychic":
-        return "#fb9088";
-      case "bug":
-        return "#a6c53f";
-      case "dark":
-        return "#676971";
-      case "steel":
-        return "#5595a5";
-      case "dragon":
-        return "#0875c8";
-      case "fairy":
-        return "#f09ce6";
-    }
-  };
+
   const router = useRouter();
   return (
     <div>
