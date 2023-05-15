dca5b643c590f92d9a2615dfa8978aba9e53b993
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2023-05-14 21:43:19 -0300

    pruebas

--- pages/index.js
+++ pages/index.js
@@ -71,29 +71,38 @@ const index = ({ pokemons, regiones, objetoCompleto }) => {
 };
 export async function getServerSideProps({ query }) {
   //consulta para las regiones
-  const pokemonRegion = await fetch("https://pokeapi.co/api/v2/region/");
-  const pokemonRegionJson = await pokemonRegion.json();
+  try {
+    const pokemonRegion = await fetch("https://pokeapi.co/api/v2/region/");
+    const pokemonRegionJson = await pokemonRegion.json();
 
-  //consulta para los pokemons
-  let pokemonsList = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/?offset${query.offset}&limit=30`
-  );
-  const pokemonsJSON = await pokemonsList.json();
+    //consulta para los pokemons
+    let pokemonsList = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?offset${query.offset}&limit=30`
+    );
+    const pokemonsJSON = await pokemonsList.json();
 
-  const pokemonsData = await Promise.all(
-    pokemonsJSON.results.map(async ({ url }) => {
-      const data = await fetch(url);
-      const dataJSON = await data.json();
-      return dataJSON;
-    })
-  );
-
-  return {
-    props: {
-      pokemons: pokemonsData,
-      regiones: pokemonRegionJson.results,
-      objetoCompleto: pokemonsJSON,
-    },
-  };
+    const pokemonsData = await Promise.all(
+      pokemonsJSON.results.map(async ({ url }) => {
+        const data = await fetch(url);
+        const dataJSON = await data.json();
+        return dataJSON;
+      })
+    );
+    return {
+      props: {
+        pokemons: pokemonsData,
+        regiones: pokemonRegionJson.results,
+        objetoCompleto: pokemonsJSON,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        pokemons: [],
+        regiones: [],
+        objetoCompleto: [],
+      },
+    };
+  }
 }
 export default index;
